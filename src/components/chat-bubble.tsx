740637685bd71de18d6cd789910ef75261247c05
DiazNugraha <diaznugraha00@gmail.com>
2023-05-18 16:24:52 +0700

    add bubble chat component and add headless ui

---
+++ src/components/chat-bubble.tsx
@@ -0,0 +1,41 @@
+import { twMerge } from "tailwind-merge";
+
+type position = "left" | "right";
+
+interface ChatBubbleProps {
+  children: React.ReactNode;
+  className?: string;
+  position: position;
+}
+
+const __DEFAULT_ELEMENT__ = "div";
+
+export default function ChatBubble({
+  className,
+  position,
+  children,
+}: ChatBubbleProps) {
+  const Component = __DEFAULT_ELEMENT__;
+
+  return (
+    <Component
+      className={`w-full h-auto flex items-center py-2 px-6 ${
+        position == "left" ? "justify-start" : "justify-end"
+      }`}
+    >
+      <Component
+        className={twMerge(
+          `w-fit p-3 bg-white flex-col`,
+          className,
+          position == "left"
+            ? "rounded-r-lg rounded-bl-lg"
+            : "rounded-l-lg rounded-br-lg"
+        )}
+      >
+        <div className={`text-lg font-bold ${position == "left" ? "text-left" : "text-right"}`}>Sender Name</div>
+        <div>{children}</div>
+        <div className={`text-xs text-gray-400 ${position == "left" ? "text-left" : "text-right"}`}>16.08 Thursday</div>
+      </Component>
+    </Component>
+  );
+}
--- src/pages/chatroom/index.tsx
+++ src/pages/chatroom/index.tsx
@@ -1,4 +1,5 @@
 import Button from "@/components/button";
+import ChatBubble from "@/components/chat-bubble";
 import PaperPlaneIcon from "@/components/icons/paper-plane";
 import TextField from "@/components/text-input";
 
@@ -7,7 +8,14 @@ export default function Chatroom() {
     <div className="flex h-screen w-full justify-center items-center">
       <div className="h-full w-full bg-[#6B7BD3] flex justify-center items-center">
         <div className="h-3/4 w-2/5 bg-[#434240] rounded-lg flex flex-col items-center p-6">
-          <div className="w-full h-full"></div>
+          <div className="w-full h-full bg-gray-300 gap-y-5">
+            <ChatBubble position="right">
+              right Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, vel.
+            </ChatBubble>
+            <ChatBubble position="left">
+              left Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, itaque sapiente! Pariatur sed distinctio reprehenderit natus impedit ab quibusdam quaerat.
+            </ChatBubble>
+          </div>
           <div className="w-full h-1/6 flex justify-center items-center">
             <div className="w-full">
               <TextField className="bg-white p-6 mt-8" size="large" />
