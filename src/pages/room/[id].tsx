79177ff75ca98c2e7cb609518d8c2fd5f1844eea
DiazNugraha <diaznugraha00@gmail.com>
2024-07-25 01:33:28 +0700

    feat: client chat room

--- src/pages/room/[id].tsx
+++ src/pages/room/[id].tsx
@@ -1,34 +1,14 @@
-import Button from "@/components/button";
-import TextField from "@/components/text-input";
+import Chatroom from "@/components/chatroom/chatroom";
 import { useRouter } from "next/router";
 
 export default function EditRoom() {
   const router = useRouter();
-  const handleBack = () => {
-    router.back();
-  }
+  const { id } = router.query;
 
   return (
     <div className="flex h-screen w-full flex-col justify-center items-center">
-      <div className="w-[1109px] flex justify-end">
-        <Button className="mb-5" onClick={handleBack}>Back</Button>
-      </div>
-      <div className="w-[1109px] bg-[#f3f2f2] rounded-lg flex flex-col items-center p-6 gap-y-3">
-        <h1 className="text-2xl">Edit your story</h1>
-        <TextField
-          type="text"
-          placeholder="Enter your title"
-          className="w-[766px] bg-white"
-        />
-        <TextField
-          type="text"
-          placeholder="Enter your description"
-          className="w-[766px] bg-white"
-          multiline
-        />
-        <hr className="border-1 border-slate-200 w-[766px]" />
-
-        <Button className="w-[766px]">Save</Button>
+      <div className="h-full w-full bg-[#6B7BD3] flex justify-center items-center">
+        <div className="h-3/4 w-2/5">{id && <Chatroom id={String(id)} />}</div>
       </div>
     </div>
   );
